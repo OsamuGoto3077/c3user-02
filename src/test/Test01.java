39544b66bbcf069e2924f2f8a604c2e81c1779d0
OsamuGoto <osagoto@sea.plala.or.jp>
2025-10-06 10:52:11 +0900

    新規作成

---
+++ src/test/Test01.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 1; i <= 9; i++) {
+			sum += i;
+		}
+		System.out.println("1～9の合計＝" + sum);
+	}
+
+}
