b42d914e199b7ed914f2dd56c3576d1e96f954b5
OsamuGoto <osagoto@sea.plala.or.jp>
2025-10-08 13:09:32 +0900

    ループ毎の合計表示を削除

--- src/test/Test01.java
+++ src/test/Test01.java
@@ -6,7 +6,7 @@ public class Test01 {
 		int sum = 0;
 		for (int i = 1; i <= 9; i++) {
 			sum += i;
-			System.out.println(i + "回目の合計＝" + sum);
+			// System.out.println(i + "回目の合計＝" + sum);
 		}
 		System.out.println("1～9の合計＝" + sum);
 	}
